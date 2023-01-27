67ab3186adc4b5c87d41b4009d9a4307be5a7816
Chiriko <sofia.digennarox@gmail.com>
2023-01-27 16:56:23 +0100

    Marker and info added

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-map',
@@ -6,6 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent {
-  center: google.maps.LatLngLiteral = {lat: 44.4949, lng: 11.3426};
+  INITIAL_COORDS = [44.4949, 11.3426]; // Bologna
+  center: google.maps.LatLngLiteral = { lat: this.INITIAL_COORDS[0], lng: this.INITIAL_COORDS[1]};
   zoom = 4;
+
+  markers = [
+  {
+    position: { lat: 38.9987208, lng: -77.2538699 },
+    info: {
+      name: "Great Falls",
+      direction: "Potomac, MD 20854, United States"
+    }
+  },
+  {
+    position: { lat: 39.7, lng: -76.0 },
+    info: {
+      name: "Grove Farm Wildlife Management Area",
+      direction: "1989-2103 Grove Neck Rd, Earleville, MD 21919, United States"
+    }
+  },
+  {
+    position: { lat: 37.9, lng: -76.8 },
+    info: {
+      name: "Virginia",
+      direction: "Virginia, United States"
+    }
+  },
+  {
+    position: { lat: this.INITIAL_COORDS[0], lng: this.INITIAL_COORDS[1]},
+    info: {
+      name: "Bologna",
+      direction: "Bologna, Italy"
+    }
+  }
+]
+
+public openInfoWindow(marker: MapMarker, infoWindow: MapInfoWindow) {
+  infoWindow.open(marker);
+}
 }
