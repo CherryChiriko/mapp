0e036da04a07545b7f18aa2cfefac642be3cd228
Chiriko <sofia.digennarox@gmail.com>
2023-01-30 11:08:00 +0100

    Distance calculation function added

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,18 +11,31 @@ export class MapComponent implements OnInit{
   INITIAL_COORDS = [44.4949, 11.3426]; // Bologna
   center: google.maps.LatLngLiteral = { lat: this.INITIAL_COORDS[0], lng: this.INITIAL_COORDS[1]};
   zoom = 4;
-  
+
   private URL = '../assets/data.json';
   constructor(private http: HttpClient){}
   
-  markers !: any;
+  markers !: any;  // Fare un'interfaccia quando conosciamo la struttura del JSON
 
   ngOnInit(){
-    this.http.get(this.URL).subscribe( dat=> this.markers = dat )
+    this.http.get(this.URL).subscribe( dat => this.markers = dat )
+    this.distanceCalc();
   }
 
+  openInfoWindow(marker: MapMarker, infoWindow: MapInfoWindow) {
+    infoWindow.open(marker);
+  }
 
-public openInfoWindow(marker: MapMarker, infoWindow: MapInfoWindow) {
-  infoWindow.open(marker);
-}
+  latitude1 = 39.46;
+  longitude1 = -0.36;
+  latitude2 = 40.40;
+  longitude2 = -3.68;
+
+  distanceCalc(){
+    var distance = google.maps.geometry.spherical.computeDistanceBetween(
+      new google.maps.LatLng(this.latitude1, this.longitude1), 
+      new google.maps.LatLng(this.latitude2, this.longitude2)
+      );
+    console.log(distance)  // answer is in meters
+  }
 }
