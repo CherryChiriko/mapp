ecba338a9fe96fe1f1398b71ed5028aec96b3b4a
Giulio Cutruneo <gcutruneo@gmail.com>
2023-02-01 15:09:22 +0100

    service excel implemented with observable

--- src/app/services/excel.service.ts
+++ src/app/services/excel.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
 import * as XLSX from 'xlsx';
-import { IClient } from '../interfaces/iclient';
+import { ISpecialist } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +10,22 @@ export class ExcelService {
 
   constructor() { }
 
-  public excelData ?: IClient[];
+  public _excelData : ISpecialist[] = [];
+  private excelData$ = new ReplaySubject<ISpecialist[]>(1);
+
+  private get excelData() {
+    return this._excelData;
+  }
+
+  private set excelData(value : ISpecialist[]) {
+    this._excelData = value;
+    this.excelData$.next(value);
+    }
 
   public loadExcelFile(event : any) {
     const file = event.target.files[0];
     const fileReader = new FileReader();
     fileReader.readAsBinaryString(file);
-
     fileReader.onload = (e) => {
       console.log(e);
       let workBook = XLSX.read(fileReader.result, {type: 'binary'});
@@ -24,7 +34,12 @@ export class ExcelService {
     }
   }
 
-  public getAll() {
-    return this.excelData;
+  public getAll() : Observable<ISpecialist[]> {
+    return this.excelData$;
+  }
+
+  public addCity(value : ISpecialist) {
+    this._excelData.push(value);
+    this.excelData = this.excelData;
   }
 }
