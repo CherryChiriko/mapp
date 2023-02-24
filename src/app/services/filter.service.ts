e75cae049e1065fb85593c588855f7b876df31f8
Giulio Cutruneo <gcutruneo@gmail.com>
2023-02-24 10:03:40 +0100

    format filterService

--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, combineLatest, map, Observable, ReplaySubject } from 'rxjs';
-import { ICFilter, IClient, ISFilter, ISpecialist } from '../interfaces/interfaces';
+import {
+  BehaviorSubject,
+  combineLatest,
+  map,
+  Observable,
+  ReplaySubject,
+} from 'rxjs';
+import {
+  ICFilter,
+  IClient,
+  ISFilter,
+  ISpecialist,
+} from '../interfaces/interfaces';
 
 import sData from 'src/assets/specialists.json';
 import cData from 'src/assets/clients.json';
@@ -10,8 +21,7 @@ import { FormService } from './form.service';
 @Injectable({
   providedIn: 'root',
 })
-export class FilterService{
-
+export class FilterService {
   clientArray: IClient[] = [];
   specialistArray: ISpecialist[] = [];
 
@@ -22,9 +32,6 @@ export class FilterService{
   sFilterSubj$ = new ReplaySubject<ISFilter>(1);
 
   constructor(private form: FormService) {
-
-
-
     this.clientArray = this.form.formatClientArr(cData);
     this.clients$.next(this.clientArray);
     this.specialistArray = this.form.formatSpecialistArr(sData);
@@ -32,18 +39,22 @@ export class FilterService{
     this.resetAllFilters();
   }
 
-  initSpecialist(specialist : ISpecialist[]) {
+  initSpecialist(specialist: ISpecialist[]) {
     this.specialistArray = this.form.formatSpecialistArr(specialist);
     this.specialists$.next(this.specialistArray);
   }
 
-  initClient(client : IClient[]) {
+  initClient(client: IClient[]) {
     this.clientArray = this.form.formatClientArr(client);
     this.clients$.next(this.clientArray);
   }
 
-  getClient() : IClient[] {    return this.clientArray;  }
-  getSpecialist() : ISpecialist[]{    return this.specialistArray;  }
+  getClient(): IClient[] {
+    return this.clientArray;
+  }
+  getSpecialist(): ISpecialist[] {
+    return this.specialistArray;
+  }
 
   resetCFilter() {
     const emptyFilter = {
@@ -69,45 +80,46 @@ export class FilterService{
     };
     this.setSFilter(emptyFilter);
   }
-  resetAllFilters() {    this.resetCFilter();    this.resetSFilter();  }
+  resetAllFilters() {
+    this.resetCFilter();
+    this.resetSFilter();
+  }
 
   addClient(newClient: IClient) {
-    if(this.checkClient(newClient.name, newClient.city))
-    {      alert("Client " + newClient.name + " is already present in " + newClient.city)    }
-    else{
+    if (this.checkClient(newClient.name, newClient.city)) {
+      alert('Client ' + newClient.name + ' is already present in ' + newClient.city);
+    } else {
       this.clientArray.push(newClient);
       this.clients$.next(this.clientArray);
     }
   }
 
   addSpecialist(newSpecialist: ISpecialist) {
-    if(this.checkSpecialist(newSpecialist.id)){
-      alert("Consultant with ID " + newSpecialist.id + " is already present")
-    }else{
-    this.specialistArray.push(newSpecialist);
-    this.specialists$.next(this.specialistArray);
+    if (this.checkSpecialist(newSpecialist.id)) {
+      alert('Consultant with ID ' + newSpecialist.id + ' is already present');
+    } else {
+      this.specialistArray.push(newSpecialist);
+      this.specialists$.next(this.specialistArray);
     }
   }
 
-  checkSpecialist(id : string) : boolean {
-    let matchUser = this.specialistArray.find((spec) =>
-    id === spec.id)
-    if(matchUser){
+  checkSpecialist(id: string): boolean {
+    let matchUser = this.specialistArray.find((spec) => id === spec.id);
+    if (matchUser) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
 
-  checkClient(name : string, city : string) : boolean{
+  checkClient(name: string, city: string): boolean {
     let matchClient = this.clientArray.find((client) => name === client.name);
-    if(matchClient?.city === city){
+    if (matchClient?.city === city) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
-
 
   removeClient(client: IClient) {
     let index = this.clientArray.indexOf(client);
@@ -124,18 +136,27 @@ export class FilterService{
     this.specialists$.next(this.specialistArray);
   }
 
-  setSFilter(filt: ISFilter) {    this.sFilterSubj$.next(filt);  }
-  setCFilter(filt: ICFilter) {    this.cFilterSubj$.next(filt);  }
+  setSFilter(filt: ISFilter) {
+    this.sFilterSubj$.next(filt);
+  }
+  setCFilter(filt: ICFilter) {
+    this.cFilterSubj$.next(filt);
+  }
 
-  createFilter(filter: any, isClient: boolean){
+  createFilter(filter: any, isClient: boolean) {
     const val = filter;
-    const degreeArr : string[] = this.form.convertToArray(val, "degree");
-    const specialtiesArr : string[] = this.form.convertToArray(val, "specialties");
-    const interestsArr : string[] = this.form.convertToArray(val, "interests");
-    const regionsArr : string[] = this.form.convertToArray(val, "regions")
-    const date = val.end? [this.form.formatDate(val.start),this.form.formatDate(val.end)]: null;
+    const degreeArr: string[] = this.form.convertToArray(val, 'degree');
+    const specialtiesArr: string[] = this.form.convertToArray(
+      val,
+      'specialties'
+    );
+    const interestsArr: string[] = this.form.convertToArray(val, 'interests');
+    const regionsArr: string[] = this.form.convertToArray(val, 'regions');
+    const date = val.end
+      ? [this.form.formatDate(val.start), this.form.formatDate(val.end)]
+      : null;
 
-    if (isClient){
+    if (isClient) {
       let clientFilter: ICFilter = {
         name: val.name,
         region: regionsArr,
@@ -143,12 +164,11 @@ export class FilterService{
         lookFor: specialtiesArr,
         // interests: interestsArr,
         available_from: date,
-        notice: val.notice
-      }
-      console.log(clientFilter)
+        notice: val.notice,
+      };
+      console.log(clientFilter);
       this.setCFilter(clientFilter);
-    }
-    else {
+    } else {
       let specialistFilter: ISFilter = {
         name: val.name,
         region: regionsArr,
@@ -157,8 +177,8 @@ export class FilterService{
         skills: specialtiesArr,
         interests: interestsArr,
         available_from: date,
-        notice: val.notice
-      }
+        notice: val.notice,
+      };
       this.setSFilter(specialistFilter);
     }
   }
@@ -166,14 +186,20 @@ export class FilterService{
   //                 Filter logic
 
   cFilterLogic(arr: IClient[], filt: ICFilter): IClient[] {
-    if (!filt) {      return arr;    }
+    if (!filt) {
+      return arr;
+    }
     let result = arr;
     for (const [key, value] of Object.entries(filt)) {
-      if (value === null) {        continue;      }
+      if (value === null) {
+        continue;
+      }
       let keyName = key as keyof IClient;
       // console.log('I am the result so far', result, keyName);
       if (Array.isArray(value)) {
-        if (!value.length) {          continue;        }
+        if (!value.length) {
+          continue;
+        }
         result = result.filter((element: any) =>
           value.some((el) => element[keyName].includes(el))
         );
@@ -188,16 +214,21 @@ export class FilterService{
     return result;
   }
 
-
   sFilterLogic(arr: ISpecialist[], filt: ISFilter): ISpecialist[] {
-    if (!filt) {      return arr;    }
+    if (!filt) {
+      return arr;
+    }
     let result = arr;
     for (const [key, value] of Object.entries(filt)) {
-      if (value === null) {        continue;      }
+      if (value === null) {
+        continue;
+      }
       let keyName = key as keyof ISpecialist;
       // console.log('I am the result so far', result, keyName);
       if (Array.isArray(value)) {
-        if (!value.length) { continue; }
+        if (!value.length) {
+          continue;
+        }
         result = result.filter((element: any) =>
           value.some((el) => element[keyName].includes(el))
         );
@@ -223,7 +254,8 @@ export class FilterService{
   sFilterData(): Observable<ISpecialist[]> {
     return combineLatest([this.specialists$, this.sFilterSubj$]).pipe(
       map(([specialist, filterValue]) =>
-        this.sFilterLogic(specialist, filterValue))
+        this.sFilterLogic(specialist, filterValue)
+      )
     );
   }
 }
