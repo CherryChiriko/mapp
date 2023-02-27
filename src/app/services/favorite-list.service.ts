578fef407412cfad5dbd089b472c6897d0bbe0bf
Chiriko <sofia.digennarox@gmail.com>
2023-02-27 14:53:01 +0100

    Infobox complete
    Have to fix color and markers for 'need'

--- src/app/services/favorite-list.service.ts
+++ src/app/services/favorite-list.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { ISpecialist } from '../interfaces/interfaces';
+import { HelperService } from './helper.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoriteListService {
 
-  constructor() { }
+  constructor(private helper: HelperService) { }
 
   public favoriteSArray : ISpecialist[] = [];
   public favoriteSpecialist$ = new BehaviorSubject<ISpecialist[]>([]);
@@ -15,18 +16,11 @@ export class FavoriteListService {
   public addSpecialistFavorite(specialist : ISpecialist){
     this.favoriteSArray.push(specialist);
     this.favoriteSpecialist$.next(this.favoriteSArray);
-    console.log(this.favoriteSArray);
-
   }
 
   public removeFavoriteSpecialist(specialist : ISpecialist){
-    let index = this.favoriteSArray.indexOf(specialist);
-    if(index > -1) {
-      this.favoriteSArray.splice(index, 1);
-    }
+    this.helper.removeElement(specialist, this.favoriteSArray);
     this.favoriteSpecialist$.next(this.favoriteSArray);
-    console.log(this.favoriteSArray);
-
   }
 
   public getFavoriteList() : Observable<ISpecialist[]> {
