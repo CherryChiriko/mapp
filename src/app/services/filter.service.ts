c7beaea071b00db99384dc9701d43ab5e299c9e8
Chiriko <sofia.digennarox@gmail.com>
2023-03-02 11:56:03 +0100

    Filter client works, restructuring in progress

--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -138,18 +138,17 @@ export class FilterService {
   }
 
   createFilter(filter: any, isClient: boolean) {
-    console.log("I am filter service")
     const val = filter;
     // const interestsArr : string[] = this.form.convertToArray(val, "interests");
+    const activitiesArr : string[] = this.form.convertToArray(val, "activities");
     // this.checkedRegions = [];
-    console.log(val.interests)
     const date = this.helper.addDays(val.date);
 
     if (isClient){
       let clientFilter: ICFilter = {
         name: val.name,
         bm: val.bm,
-        activities: val.interests,
+        activities: activitiesArr,
         need: val.need
       }
       console.log(clientFilter)
@@ -171,28 +170,66 @@ export class FilterService {
 
   //                 Filter logic
 
-  cFilterLogic(arr: IClient[], filt: ICFilter): IClient[] {
+  filterLogic(arr: Object[], filt: Object){
     if (!filt) {      return arr;    }
     let result = arr;
     for (const [key, value] of Object.entries(filt)) {
       if (value === null) {        continue;      }
       let keyName = key as keyof IClient;
+      if (keyName === 'need') {    continue;      }
       console.log('I am the result so far', result, keyName);
-      if (Array.isArray(value)) {
-        if (!value.length) {          continue;        }
-        result = result.filter((element: any) =>
-          value.some((el) => element[keyName].includes(el))
-        );
-        continue;
-      } 
-      else {
-        let reg = new RegExp(value, 'i');
-        result = result.filter((element: any) => reg.test(element[keyName]));
-        console.log('and I am the result ', result);
-        continue;
-      }
-    }
+      switch (true) {
+        case Array.isArray(value): {
+          if (!value.length) { continue;  }
+          if (keyName === 'activities' && filt.need) {
+            result = result.filter((element: any) =>
+            value.some((el: any) => element.need.includes(el))); 
+            break;
+          }
+          result = result.filter((element: any) =>
+          value.some((el: any) => element[keyName].includes(el))
+          );
+          break;
+        }
+        default: {
+          let reg = new RegExp(value, 'i');
+          result = result.filter((element: any) => reg.test(element[keyName]));
+          console.log('and I am the result ', result);
+          break;
+        }
+      }}
     return result;
+  }
+  cFilterLogic(arr: IClient[], filt: ICFilter): IClient[] {
+    return this.filterLogic(arr, filt);
+    // if (!filt) {      return arr;    }
+    // let result = arr;
+    // for (const [key, value] of Object.entries(filt)) {
+    //   if (value === null) {        continue;      }
+    //   let keyName = key as keyof IClient;
+    //   if (keyName === 'need') {    continue;      }
+    //   console.log('I am the result so far', result, keyName);
+    //   switch (true) {
+    //     case Array.isArray(value): {
+    //       if (!value.length) { continue;  }
+    //       if (keyName === 'activities' && filt.need) {
+    //         result = result.filter((element: any) =>
+    //         value.some((el: any) => element.need.includes(el))); 
+    //         break;
+    //       }
+    //       result = result.filter((element: any) =>
+    //       value.some((el: any) => element[keyName].includes(el))
+    //       );
+    //       break;
+    //     }
+    //     default: {
+    //       let reg = new RegExp(value, 'i');
+    //       result = result.filter((element: any) => reg.test(element[keyName]));
+    //       console.log('and I am the result ', result);
+    //       break;
+    //     }
+    //   }}
+    // return result;
   }
 
   sFilterLogic(arr: ISpecialist[], filt: ISFilter): ISpecialist[] {
